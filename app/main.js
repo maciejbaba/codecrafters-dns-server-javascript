225ad79e54bf6f94595f60a6fc3abafef31e8a5f
Maciej Baba <maciek2baba2@gmail.com>
2025-01-26 13:48:53 +0100

    create header

--- app/main.js
+++ app/main.js
@@ -8,9 +8,21 @@ console.log("Logs from your program will appear here!");
 const udpSocket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
 
+function createDNSHeader(queryBuf) {
+  const header = Buffer.alloc(12);
+  header.writeUInt16BE(1234, 0); // packet id
+  header.writeUInt16BE(0x8000, 2); // flags
+  header.writeUInt16BE(0, 4); // question count
+  header.writeUInt16BE(0, 6); // answer count
+  header.writeUInt16BE(0, 8); // authority count
+  header.writeUInt16BE(0, 10); // additional count
+
+  return header;
+}
+
 udpSocket.on("message", (buf, rinfo) => {
   try {
-    const response = Buffer.from("");
+    const response = createDNSHeader(buf);
     udpSocket.send(response, rinfo.port, rinfo.address);
   } catch (e) {
     console.log(`Error receiving data: ${e}`);
