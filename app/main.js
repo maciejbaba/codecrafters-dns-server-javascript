a90d429995a825212d3678cf6f9533a038ea8a68
Maciej Baba <maciek2baba2@gmail.com>
2025-01-26 21:43:08 +0100

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -8,10 +8,22 @@ class DNS {
     this.domain = domain;
   }
 
-  createDNSHeader() {
+  createDNSHeader(incomingHeader) {
     const header = Buffer.alloc(12);
-    header.writeUInt16BE(1234, 0); // packet id
-    header.writeUInt16BE(0x8000, 2); // flags
+    header.writeUInt16BE(incomingHeader.packetId, 0); // packet id
+
+    let flags = 0;
+    flags |= (1 << 15);
+    flags |= (incomingHeader.operationCode << 11);
+    flags |= (0 << 10);
+    flags |= (0 << 9);
+    flags |= (incomingHeader.recursionDesired << 8);
+    flags |= (0 << 7);
+    flags |= (0 << 4);
+    flags |= (incomingHeader.operationCode === 0 ? 0 : 4 << 0);
+    header.writeUInt16BE(flags, 2);
+
+
     header.writeUInt16BE(1, 4); // question count
     header.writeUInt16BE(1, 6); // answer count
     header.writeUInt16BE(0, 8); // authority count
@@ -57,17 +69,50 @@ class DNS {
 
     return Buffer.concat([domainBuffer, rest]);
   }
+
+  parseHeader(buf, offset) {
+    const packetId = buf.readUInt16BE(offset);
+    const thirdByte = buf.readUInt8(offset + 2);
+    const queryOrResponseIndicator = (thirdByte >> 7) & 0b00000001;
+    const operationCode = (thirdByte >> 3) & 0b00001111;
+    const authoritativeAnswer = (thirdByte >> 2) & 0b00000001;
+    const truncation = (thirdByte >> 1) & 0b00000001;
+    const recursionDesired = thirdByte & 0b00000001;
+    const fourthByte = buf.readUInt8(offset + 3);
+    const recursionAvailable = (fourthByte >> 7) & 0b00000001;
+    const reserved = (fourthByte >> 4) & 0b00000111;
+    const responseCode = fourthByte & 0b00001111;
+
+    const questionCount = buf.readUInt16BE(offset + 4);
+    const answerCount = buf.readUInt16BE(offset + 6);
+    const authorityCount = buf.readUInt16BE(offset + 8);
+    const additionalCount = buf.readUInt16BE(offset + 10);
+
+    return {
+      packetId,
+      queryOrResponseIndicator,
+      operationCode,
+      authoritativeAnswer,
+      truncation,
+      recursionDesired,
+      recursionAvailable,
+      reserved,
+      responseCode,
+      questionCount,
+      answerCount,
+      authorityCount,
+      additionalCount,
+    };
+  }
 }
 
 udpSocket.on('message', (buf, rinfo) => {
   try {
     const dns = new DNS('codecrafters.io');
-
-    console.log(buf);
-    console.log(rinfo);
+    const header = dns.parseHeader(buf, 0);
 
     const response = Buffer.concat([
-      dns.createDNSHeader(),
+      dns.createDNSHeader(header),
       dns.createQuestionSection(),
       dns.createAnswerSection(),
     ]);
